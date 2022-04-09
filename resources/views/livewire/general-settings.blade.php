98a1d7d40eadb500dcf1430bb2f89d80be3aab35
Randell b <58916132+randelldb@users.noreply.github.com>
2022-04-09 22:10:53 +0200

    working on settings page

--- resources/views/livewire/general-settings.blade.php
+++ resources/views/livewire/general-settings.blade.php
@@ -19,6 +19,26 @@
             <sub>This will be added on top lorem ipsum hoelahoep</sub>
         </div>
 
+        <div class="flex items-center justify-center w-full mb-12">
+  
+            <label for="toggleB" class="flex items-center cursor-pointer">
+                <!-- toggle -->
+                <div class="relative">
+                    <!-- input -->
+                    <input type="checkbox" id="toggleB" class="sr-only">
+                    <!-- line -->
+                    <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                    <!-- dot -->
+                    <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                </div>
+                <!-- label -->
+                <div class="ml-3 text-gray-700 font-medium">
+                  Toggle Me!
+                </div>
+            </label>
+          
+          </div>
+
         <div class="mb-8">
             <p class="font-light pb-2">HLS streaming bitrates (in kb/s)</p>
             <div class="grid grid-cols-3 gap-y-5">
