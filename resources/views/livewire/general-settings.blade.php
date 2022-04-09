d8e7de4e2a1a023e554eb699c30f8874624ddfd2
Randell b <58916132+randelldb@users.noreply.github.com>
2022-04-09 22:51:26 +0200

    Added toogle btns to setting modal

--- resources/views/livewire/general-settings.blade.php
+++ resources/views/livewire/general-settings.blade.php
@@ -13,31 +13,29 @@
             <sub>The name that your visitors will see</sub>
         </div>
 
+        <div class="flex flex-col items-start mb-8">
+            <div class="flex mb-4">
+                <div class="relative inline-block w-10 mr-2 align-middle select-none ">
+                    <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-status-error-500 border-2 border-background-surface appearance-none cursor-pointer"/>
+                    <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-background-surface cursor-pointer"></label>
+                </div>
+                <label for="toggle" class="font-light pb-2">Keep original file</label>
+            </div>
+
+            <div class="flex">
+                <div class="relative inline-block w-10 mr-2 align-middle select-none ">
+                    <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-status-error-500 border-2 border-background-surface appearance-none cursor-pointer"/>
+                    <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-background-surface cursor-pointer"></label>
+                </div>
+                <label for="toggle" class="font-light pb-2 ">Visitors can see video info</label>
+            </div>
+        </div>
+
         <div class="mb-8">
             <p class="font-light pb-2"><label for="retention-delay">Views retention delay in minutes</label></p>
             <p><input class="input-text w-3/4" type="text" name="retention-delay" wire:model="tag.retentionDeleay"></p>
             <sub>This will be added on top lorem ipsum hoelahoep</sub>
         </div>
-
-        <div class="flex items-center justify-center w-full mb-12">
-  
-            <label for="toggleB" class="flex items-center cursor-pointer">
-                <!-- toggle -->
-                <div class="relative">
-                    <!-- input -->
-                    <input type="checkbox" id="toggleB" class="sr-only">
-                    <!-- line -->
-                    <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
-                    <!-- dot -->
-                    <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-                </div>
-                <!-- label -->
-                <div class="ml-3 text-gray-700 font-medium">
-                  Toggle Me!
-                </div>
-            </label>
-          
-          </div>
 
         <div class="mb-8">
             <p class="font-light pb-2">HLS streaming bitrates (in kb/s)</p>
