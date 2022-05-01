43c21008dee52a34acb8af01a40a00cc17127b4e
Randell b <58916132+randelldb@users.noreply.github.com>
2022-05-01 14:44:25 +0200

    updated the media edit modal

--- resources/views/livewire/media-settings.blade.php
+++ resources/views/livewire/media-settings.blade.php
@@ -22,33 +22,41 @@
 
             <div class="flex flex-col basis-2/5"><!-- coll 2 -->
                 <div class="mb-8">
-                    <p class="font-light pb-2"><label for="mediaTitle">Media Title</label></p>
-                    <p><input class="input-text w-full" type="text" name="mediaTitle" wire:model=""></p>
-                    <sub>The title of your media</sub>
+                    <p class="font-light pb-2"><label for="mediaDiscription">Discription</label></p>
+                    <p>
+                        <textarea class="input-text w-full" name="mediaDiscription" id="mediaDiscription" cols="10" rows="5" wire:model=""></textarea>
+                    </p>
+                    <sub>Discribe your media</sub>
                 </div>
 
                 <div class="mb-8">
-                    <p class="font-light pb-2"><label for="mediaTitle">Media Title</label></p>
-                    <p><input class="input-text w-full" type="text" name="mediaTitle" wire:model=""></p>
-                    <sub>The title of your media</sub>
+                    <p class="font-light pb-2"><label for="mediaTitle">Access policy</label></p>
+                    <p>
+                        <select class="input-text w-1/3 pl-2" name="mediaAccessPolicy" id="mediaAccessPolicy" wire:model="">
+                            <option value="public">Public</option>
+                            <option value="urlOnly">URL only</option>
+                            <option value="private">Private</option>
+                        </select>
+                    </p>
+                    <sub>Select the Access policy for your media</sub>
                 </div>
 
                 <div class="mb-8">
-                    <p class="font-light pb-2"><label for="mediaTitle">Media Title</label></p>
-                    <p><input class="input-text w-full" type="text" name="mediaTitle" wire:model=""></p>
-                    <sub>The title of your media</sub>
+                    <p class="font-light pb-2"><label for="None">None</label></p>
+                    <p><input class="input-text w-full" type="text" name="None" wire:model=""></p>
+                    <sub>The None of your media</sub>
                 </div>
 
                 <div class="mb-8">
-                    <p class="font-light pb-2"><label for="mediaTitle">Media Title</label></p>
-                    <p><input class="input-text w-full" type="text" name="mediaTitle" wire:model=""></p>
-                    <sub>The title of your media</sub>
+                    <p class="font-light pb-2"><label for="None">None</label></p>
+                    <p><input class="input-text w-full" type="text" name="None" wire:model=""></p>
+                    <sub>The None of your media</sub>
                 </div>
 
                 <div class="mb-8">
-                    <p class="font-light pb-2"><label for="mediaTitle">Media Title</label></p>
-                    <p><input class="input-text w-full" type="text" name="mediaTitle" wire:model=""></p>
-                    <sub>The title of your media</sub>
+                    <p class="font-light pb-2"><label for="None">None</label></p>
+                    <p><input class="input-text w-full" type="text" name="None" wire:model=""></p>
+                    <sub>The None of your media</sub>
                 </div>
             </div>
         </div>
